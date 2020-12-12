fdc4eadb11dc9b44b2ce84dd6b5acd891e48fad9
SRWSeb <65510662+SRWSeb@users.noreply.github.com>
2020-12-12 12:47:32 +0100

    Further Mustache work

--- test.php
+++ test.php
@@ -7,9 +7,12 @@
     public $standings = array();
     public $disqualified = array();
 
-    public function __construct($seasonInfo) {
+    public function __construct() {
 
-      for ($i=1; $i <= $seasonInfo['rounds']; $i++) {
+      $args = func_get_args();
+      var_export($args);
+
+      /*for ($i=1; $i <= $seasonInfo['rounds']; $i++) {
         $round = 'Round '. $i;
         array_push($this->rounds, $round);
       }
@@ -28,8 +31,31 @@
         'league_name' => $seasonInfo['league_name'],
         'season_id' => $seasonInfo['id'],
         'season_name' => $seasonInfo['season_name']
+      );*/
+
+    }
+
+    public function loadSeason($seasonID) {
+      for ($i=1; $i <= 10; $i++) {
+        $round = 'Round '. $i;
+        array_push($this->rounds, $round);
+      }
+
+      $this->standings = array(
+        0 => array('position' => '1', 'name' => 'Johnny Verhoeff', 'driverID' => '66', 'class' => '0', 'car' => 'Ford GTE', 'total_inc' => 0, 'inc_color' => '#33cc33', 'total_pts' => 185, 'active' => '1', 'rounds' => array(80,105,0,0,0,0,0,0,0,0) ),
+        1 => array('position' => '2', 'name' => 'Bram van Putten', 'driverID' => '64', 'class' => '1', 'car' => 'BMW M8 GTE', 'total_inc' => 13, 'inc_color' => '#ff9900','total_pts' => 180, 'active' => '1', 'rounds' => array(90,90,0,0,0,0,0,0,0,0) )
       );
 
+      $this->disqualified = array(
+        0 => array('name' => 'Rutger Wijnen', 'driverID' => '66', 'class' => '0', 'car' => 'Ford GTE', 'total_inc' => 0, 'inc_color' => '#33cc33', 'total_pts' => 185, 'active' => '1', 'rounds' => array(80,105,0,0,0,0,0,0,0,0) ),
+        1 => array('name' => 'Jeromy Hessels', 'driverID' => '64', 'class' => '1', 'car' => 'BMW M8 GTE', 'total_inc' => 13, 'inc_color' => '#ff9900','total_pts' => 180, 'active' => '1', 'rounds' => array(90,90,0,0,0,0,0,0,0,0) )
+      );
+
+      $this->season = array(
+        'league_name' => 'Simracersworld GT Series',
+        'season_id' => 3,
+        'season_name' => 'PRO Season 7'
+      );
     }
 
 
@@ -39,8 +65,8 @@
   $champCtrl = new ChampCtrl();
   $seasonInfo = $champCtrl->getSeasonInfo($_GET['season']);
   $s = new Standings($seasonInfo);
+  $s->loadSeason(3);
 
-  var_export($seasonInfo);
   echo $m->render('standings', $s);
 
   require "footer.php";
