9e215c60c4353f2bda3b41f0df85cb619a73e310
SRWSeb <sebastian.heberer@gmail.com>
2021-01-19 21:00:31 +0100

    Main series on index

--- index.php
+++ index.php
@@ -15,9 +15,11 @@ $view = new UserView();
     }
   }
   ?>
-  <h1>Current series:</h1>
-  <a href="standings.php?season=7"><img src="media/BMW_LOGO.png" class="img-fluid"></a>
-  <a href="standings.php?season=6"><img src="media/NASCAR_LOGO.png" class="img-fluid"></a>
+  <div class="jumbotron text-center">
+  <h1>Current series</h1>
+  <a href="standings.php?season=7"><img src="media/BMW_LOGO.png" class="img-fluid pt-3"></a>
+  <a href="standings.php?season=6"><img src="media/NASCAR_LOGO.png" class="img-fluid pt-3"></a>
+  </div>
 </div>
 
 <?php
